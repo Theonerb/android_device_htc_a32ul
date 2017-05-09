72e11f48362c0d264f75473175af878c63af5e22
tarkzim <tom8476oo@gmail.com>
2017-05-10 01:03:37 +0800

    a32ul: add lineage device makefile

---
+++ lineage.mk
@@ -0,0 +1,30 @@
+#
+# Copyright (C) 2015 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+$(call inherit-product, device/htc/a32ul/device.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+PRODUCT_NAME := lineage_a32ul
+PRODUCT_DEVICE := a32ul
+PRODUCT_BRAND := htc
+PRODUCT_MANUFACTURER := HTC
+PRODUCT_MODEL := Desire 626
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_FINGERPRINT="htc/a32ul_htc_asia_tw/htc_a32ul:4.4.4/KTU84P/508422.2:user/release-keys" \
+    PRIVATE_BUILD_DESC="1.10.709.2 CL508422 release-keys"
